ddaaaec73bdb9e86ff8d0f6f2fb4417f8f18f0ae
Artim Abalmaz <muell2015@gmail.com>
2025-03-18 12:04:18 +0300

    Add project files.

---
+++ DrawTogether/Hubs/DrawHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace DrawTogether.Hubs
+{
+    public class DrawHub : Hub
+    {
+        public async Task SendDraw(float startX, float startY, float endX, float endY)
+        {
+            await Clients.Others.SendAsync("ReceiveDraw", startX, startY, endX, endY);
+        }
+    }
+}
